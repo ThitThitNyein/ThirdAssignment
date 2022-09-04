5c49089195cff4725cd527c9e21554b513e32d96
ThitThitNyein <85009798+ThitThitNyein@users.noreply.github.com>
2022-09-04 23:17:46 +0800

    Update ThirdAssignment.c

--- ThirdAssignment.c
+++ ThirdAssignment.c
@@ -1,24 +1,24 @@
 # ThirdAssignment
 /*For exam*/
-#include<studio.h>
-into main()
+#include<stdio.h>
+int main()
 {
  int x[5]={};
  int i;
  int sum=0;
  into *ptr;
 
- printf(Enter 5 numbers : );
+ printf("Enter 5 numbers : ");
  for(i=0;i<5;++i)
  {
-   printf(("&x[%d]=%p\n",i,&x[i]);
-   scanf(%d",x+i);
+   printf("&x[%d]=%p\n",i,&x[i]);
+   scanf("%d",x+i);
    sum+=*(x+i);
   }
  printf("Address of array x: %p",x);
  printf("Sum = %d",sum);
 
- ptr=&x[4];
+ ptr=&x[3];
  printf("*ptr=%d\n",*ptr);
  printf("*(ptr+1)=%d\n",*(ptr+1));
  printf("*(ptr-1)=%d\n",*(ptr-1));
